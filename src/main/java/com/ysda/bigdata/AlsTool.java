24b49b5b13a9dfec5b61a172cdfe27e7ecaed782
Klimovich Andrei <andrey_klim@bk.ru>
2016-04-12 14:51:45 +0300

    Add empty project

---
+++ src/main/java/com/ysda/bigdata/AlsTool.java
@@ -0,0 +1,10 @@
+package com.ysda.bigdata;
+
+/**
+ * Created by xakl on 12.04.2016.
+ */
+public class AlsTool {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
